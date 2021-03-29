82145e9d7b4a9c890869fb1be4b4c3f2e66cd71d
Cody Gramlich <cmgramli@ualberta.ca>
2021-03-28 20:27:42 -0600

    Move postgres to its own repo

--- postgres/scripts/2.data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (email, username, password) VALUES ('test_email', 'test_username', 'testpassword');
-INSERT INTO users (email, username, password) VALUES ('test_email2', 'test_username2', 'testpassword2');
