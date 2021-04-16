3e03719d7861d033f5222718e21c7aa4cfbaeb1f
Cody Gramlich <cmgramli@ualberta.ca>
2021-04-16 14:29:21 -0600

    tests working

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,28 +7,22 @@ from app import database
 from app import create_app
 from dotenv import load_dotenv
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app():
     load_dotenv()
-
-    db_fd, db_path = tempfile.mkstemp()
-    print(db_path)
-    app = create_app({"TESTING": True}, 'sqlite:///' + db_path)
+    app = create_app({"TESTING": True}, 'sqlite:///:memory:')
 
     yield app
 
-    os.close(db_fd)
-    os.unlink(db_path)
-
-
-@pytest.fixture
+@pytest.fixture(scope='session')
 def client(app):
     return app.test_client()
 
 @pytest.fixture
 def session(app):
     from app.database import session
-    return session
+    yield session
+    session.execute("DELETE FROM users")
 
 @pytest.fixture
 def auth(client):
--- tests/test_verify_reset_token.py
+++ tests/test_verify_reset_token.py
@@ -1,27 +1,27 @@
 import pytest
 
-# def test_verify_reset_token_missing_token(client, app, session, auth):
-#     resp = auth.register()
-#     assert resp.status_code == 200
+def test_verify_reset_token_missing_token(client, app, session, auth):
+    resp = auth.register()
+    assert resp.status_code == 200
 
-#     body = {
-#         "reset_token": ''
-#     }
-#     resp = client.post("/verify-reset-token", json=body)
+    body = {
+        "reset_token": ''
+    }
+    resp = client.post("/verify-reset-token", json=body)
 
-#     assert resp.status_code == 400
+    assert resp.status_code == 400
 
-#     assert resp.json["message"] == "reset token missing"
+    assert resp.json["message"] == "reset token missing"
 
-# def test_verify_reset_token_invalid_token(client, app, session, auth):
-#     resp = auth.register()
-#     assert resp.status_code == 200
+def test_verify_reset_token_invalid_token(client, app, session, auth):
+    resp = auth.register()
+    assert resp.status_code == 200
 
-#     body = {
-#         "reset_token": 'invalidtoken'
-#     }
-#     resp = client.post("/verify-reset-token", json=body)
+    body = {
+        "reset_token": 'invalidtoken'
+    }
+    resp = client.post("/verify-reset-token", json=body)
 
-#     assert resp.status_code == 400
+    assert resp.status_code == 400
 
-#     assert resp.json["message"] == "invalid token"
+    assert resp.json["message"] == "invalid token"
